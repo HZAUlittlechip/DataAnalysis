686ffe71f6fd59732633f5d798ddf6aa5bfddc07
HZAUlittlechip <157097025+HZAUlittlechip@users.noreply.github.com>
2024-12-27 20:34:29 +0800

    修改

---
+++ python_data_littlepractice/15生成数据/折线图.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+input_values = [1, 2, 3, 4, 5]
+
+# 使⽤内置样式
+# == 展示样式 ==
+print(plt.style.available)
+plt.style.use('Solarize_Light2')
+
+fix, ax = plt.subplots()
+
+#  修改标签⽂字和线条粗细
+# == 调整线宽 ==
+# == plot（x，y） ==
+ax.plot(input_values,squares,linewidth = 3)  # 都是对ax来进行的操作
+
+# == 设计图题且加上label ==
+# == fontsize 用来表示文字的大小 ==
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of Value", fontsize = 14)
+
+# == 设置刻度标记的样式 ==
+ax.tick_params(labelsize = 14)
+
+plt.show()
