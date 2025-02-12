8ebaea3a668561fdbce2c9bc756b7f84aa00e27c
HZAUlittlechip <157097025+HZAUlittlechip@users.noreply.github.com>
2025-02-12 17:04:27 +0800

    +1

--- DP_by_python_rm/code/04_神经网络的学习.py
+++ DP_by_python_rm/code/04_神经网络的学习.py
@@ -24,4 +24,19 @@ y = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
 t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
 result = cross_entropy_error(y, t)
 print(result)
+
 # mini-batch（小批量数据抓取）版本的交叉熵实现
+
+# 数值微分
+
+# 导数函数
+def numercial_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / (2 * h) # 主要分子成为2h
+
+# 偏导函数的计算演示
+def function_tmp1(x0): # 展示的函数表达
+    return x0 * x0 + 4.0**2.0
+
+numercial_diff(function_tmp1, 3.0) # 调用前面的导数求解函数
+print(numercial_diff(function_tmp1, 3.0))
